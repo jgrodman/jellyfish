77ffa69d7447a37a4f5e2d27f0253624da90a2f1
Jesse Grodman <jgrodman@myth16.Stanford.EDU>
2014-05-19 21:53:58 -0700

    Respect public/private convention

--- runme.py
+++ runme.py
@@ -2,18 +2,18 @@ from GraphGenerator import GraphGenerator
 from YenKSP.graph import DiGraph
 import os
 
-def generateGraph():
+def main():
+  g = _generateGraph()
+  _createImage(g)
+
+def _generateGraph():
   os.chdir("YenKSP")
   g = GraphGenerator(24, 5)
   return g.generate()
 
-def createImage(g):
+def _createImage(g):
   paint = g.painter()
   g.export(False, paint)
-
-def main():
-  g = generateGraph()
-  createImage(g)
 
 if __name__ == "__main__":
     main()
