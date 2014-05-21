cc4c8125a62a2010c2f9c0eee24fda166bb4f3d2
Ubuntu <ubuntu@ip-172-31-41-207.us-west-2.compute.internal>
2014-05-21 20:21:10 +0000

    Tweaked runme

--- runme.py
+++ runme.py
@@ -1,16 +1,26 @@
 from GraphGenerator import GraphGenerator
-from KSPPather import KSPPather
+from Pather import Pather
+from Figure9 import Figure9
 from YenKSP.graph import DiGraph
 import os
 
 def main():
+  _createFigure9()
+
+def _createFigure9():
+  print "===== Figure 9 ====="
+  print "Generating graph..."
   g = _generateGraph()
   _createImage(g)
-  kspPathCounts = KSPPather(g).countPaths()
-
+  print "Counting paths..."
+  p = Pather(g)
+  p.countPaths()
+  print "Creating plot..."
+  Figure9(p.kspPathCounts, p.ecmpPathCounts).plot("figure9.png")
+  
 def _generateGraph():
   os.chdir("YenKSP")
-  g = GraphGenerator(10, 5)
+  g = GraphGenerator(30, 5)
   return g.generate()
 
 def _createImage(g):
