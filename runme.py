742627cbdb7824c7f4eae5c47388d95e0164de75
Jesse Grodman <jgrodman@gmail.com>
2014-05-26 03:00:43 +0000

    Fix stuff

--- runme.py
+++ runme.py
@@ -5,33 +5,50 @@ from YenKSP.graph import DiGraph
 import os
 
 def main():
-  os.chdir("YenKSP")
-  g = _generateGraph()
-  _createImage(g)
-  os.chdir("..")
-  _createFigure9(g)
+  numSamples = 10
+  kspAverages = []
+  ecmpAverages = []
+  for i in range(0,numSamples):
+    numNodes = 35
+    edgesPerNode = 5
+    os.chdir("YenKSP")
+    graphName = "graph" + str(i)
+    g = _generateGraph(numNodes, edgesPerNode, graphName)
+    _createImage(g)
+    os.chdir("..")
+    _createFigure9(g, graphName)
+    (kspPathCounts, ecmpPathCounts) = _countPaths(g)
+    kspAverages.append(_listAverage(kspPathCounts.values()))
+    ecmpAverages.append(_listAverage(ecmpPathCounts.values()))
 
-def _createFigure9(g):
-  print "Counting paths..."
+    
+  print "KSP averages: %s" % kspAverages
+  print "ECMP averages: %s" % ecmpAverages
+   
+def _createFigure9(g, graphName):
   p = Pather(g)
   p.countPaths()
-  print "Creating plot..."
-  print len(p.kspPathCounts), len(p.ecmpPathCounts)
-  Figure9(p.kspPathCounts, p.ecmpPathCounts).plot("output/figure9.png")
+  Figure9(p.kspPathCounts, p.ecmpPathCounts).plot("output/" + graphName + "_figure9.png")
+
+
+def _countPaths(g):
+  p = Pather(g)
+  p.countPaths()
+  return (p.kspPathCounts, p.ecmpPathCounts)
   
-def _generateGraph():
+def _generateGraph(numNodes, edgesPerNode, graphName):
   print "Generating graph..."
-  numNodes = 35
-  edgesPerNode = 5
-  g = GraphGenerator(numNodes, edgesPerNode, "graph")
+  g = GraphGenerator(numNodes, edgesPerNode, graphName)
   print "numNodes", numNodes
   print "edgesPerNode", edgesPerNode
-  
   return g.generate()
 
 def _createImage(g):
   paint = g.painter()
   g.export(False, paint)
 
+def _listAverage(lst):
+  return sum(lst) / len(lst)
+  
 if __name__ == "__main__":
     main()
