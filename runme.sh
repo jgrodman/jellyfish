e8c42982267eee1ee4fbabe03f7456a42a50530a
Ubuntu <ubuntu@ip-172-31-43-134.us-west-2.compute.internal>
2014-06-01 18:57:35 +0000

    Update runme

--- runme.sh
+++ runme.sh
@@ -1,4 +1,17 @@
-# 50 graphs
-# 100 nodes
-# 24 edges per node
-python main.py 50 100 24
+# 20 graphs
+# 35 nodes
+# 5 edges per node
+python main.py 20 35 5
+mv output output_35_5
+
+# 20 graphs
+# 80 nodes
+# 20 edges per node
+python main.py 20 80 20
+mv output output_80_20
+
+# 20 graphs
+# 84 nodes
+# 12 edges per node
+python main.py 20 84 12
+mv output output_84_12
